02b3b50e02320bea57e4fff157d3c3006371944c
MartaGuzman <marta.guzmanalarcon@gmail.com>
2025-03-19 14:13:51 +0100

    2push

--- src/Sample/Sample.java
+++ src/Sample/Sample.java
@@ -3,7 +3,13 @@ package Sample;
 public class Sample {
 	
 	 public static void main(String[] args) {
-	        System.out.println("Hello, World!");
-	    }
+	     
+		 System.out.println("Hello, World!");
+	        
+	   	 System.out.println("Segundo push");
 
+	    }
 }
+
+
+
