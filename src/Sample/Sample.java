6e885e77f88ea7f3681d42779cc8b76fc43a50a7
abrilbetti <abrilbetti@gmail.com>
2025-03-20 17:53:53 +0100

    deleted sentences

--- src/Sample/Sample.java
+++ src/Sample/Sample.java
@@ -5,8 +5,7 @@ public class Sample {
 	 public static void main(String[] args) {
 	     
 	  
-	        System.out.println("haced un commit y push, para ver si funciona el pull");
-	        System.out.println("estoy haciendo cambios woohoo");
+	        
 
 
 	    }
