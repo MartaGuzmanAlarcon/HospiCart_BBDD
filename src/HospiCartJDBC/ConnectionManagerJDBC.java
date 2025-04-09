e0a3ff5c802c10af7dc5843af7010c2fa9638044
luciafandino <luciafandinogomez@gmail.com>
2025-04-09 16:17:05 +0200

    ConnectionManagerJDBC has been created and needs to be implemented

---
+++ src/HospiCartJDBC/ConnectionManagerJDBC.java
@@ -0,0 +1,13 @@
+package HospiCartJDBC;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionManagerJDBC {
+	private Connection c = null;
+	
+	
+	
+	
+}
