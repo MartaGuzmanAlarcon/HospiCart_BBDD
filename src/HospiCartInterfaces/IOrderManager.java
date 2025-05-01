a5b8e153cf617845bddbba7d641218294d96be99
abrilbetti <abrilbetti@gmail.com>
2025-05-01 20:37:52 +0200

    I created the interface of Order and the method "createOrder".

--- src/HospiCartInterfaces/IOrderManager.java
+++ src/HospiCartInterfaces/IOrderManager.java
@@ -1,5 +1,94 @@
 package HospiCartInterfaces;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import HospiCartPOJOs.Client;
+import HospiCartPOJOs.Order;
+import HospiCartPOJOs.Status;
+
+
 public interface IOrderManager {
+	/**
+	 * Method that receives the id of the user and creates an order. 
+	 * @param client
+	 */
+	//TODO: this method should establish the order_date as the present one (LOCAL DATE: i think we have to use just DATE, CHECK!) and sets its order id and status to the appropriate ones
+	//TODO: order id should be unique values and auto-increment as orders are created.
+	Order createOrder(Client client) throws SQLException;
+	
+	/**
+	 * Method that retrieves a specific order whose id matches the one received as parameter.
+	 * @param order_id integer that contains the id of the order we wish to obtain.
+	 * @return an object of Order
+	 */
+	//TODO: this method should throw an exception for the cases in which an order with the introduced id does not exist.
+	Order getOrderByID(int order_id);
+	
+	/**
+	 * Method that retrieves a list of orders whose buyer's id coincides with the id received as parameter.
+	 * @param user_id integer that stores the id of the user whose orders we want to see.
+	 * @return a list that contains all the orders associated to the user.
+	 */
+	//TODO: this method should throw an exception in case there is no user with the received id
+	List<Order> getOrdersByUser(int user_id);
+	
+	/**
+	 * Method that retrieves a list containing all the orders that were purchased on the date received as parameter.
+	 * @param order_date variable of local date type.
+	 * @return the list of orders that were purchased on the date introduced.
+	 */
+	//TODO: throw an exception if either no orders were purchased on the received date or if the date is invalid.
+	List<Order> getOrdersByOrderDate(LocalDate order_date);
+	
+	/**
+	 * Method that receives two dates as parameter, which establish the date range that is of our interest in order to filter the orders and see only the ones that fall within this range.
+	 * @param startDate variable of LocalDate type that stores the start date of the range.
+	 * @param endDate variable of LocalDate type that stores the end date of the range.
+	 * @return a list containing all the orders whose order date is between the range.
+	 */
+	//TODO: throw an exception if any of the dates is invalid or if there are no orders in the provided range.
+	List<Order> getOrdersWithinDateRange(LocalDate startDate, LocalDate endDate);
+	
+	/**
+	 * Method that receives an order id and a status as parameters and updates the status of the order whose id coincides with the received as parameter.
+	 * @param order_id integer that stores the id of the order whose status we wish to update.
+	 * @param newStatus variable of type Status that store the status we want the order to have.
+	 */
+	//TODO: this method should throw an exception for the cases in which an order with the introduced id does not exist.
+	void updateOrderStatus(int order_id, Status newStatus);
+	
+	/**
+	 * Method that receives an order's id as parameter and deletes it.
+	 * @param order_id integer that stores the id of the order we wish to remove.
+	 */
+	//TODO: this method should throw an exception for the cases in which an order with the introduced id does not exist.
+	void deleteOrder(int order_id);
+	
+	/**
+	 * Method that retrieves a list containing all the orders of HospiCart.
+	 * @return a list with all the orders.
+	 */
+	//TODO: throw an exception in case there are no orders.
+	List<Order> getAllOrders();
+	//TODO ver si es util
+	/**
+	 * Method that retrieves a list that contains all the orders whose status matches the one received as parameter.
+	 * @param status variable of type Status that contains the status we are interested in (in order to see the orders that have this status)
+	 * @return a list that contains the orders with the received status.
+	 */
+	//TODO: should this method throw an exception in case there are no orders with the specified status?
+	List<Order> getOrdersByStatus(Status status);
+	
+	/**
+	 * Method that checks whether an order exists with the order id received as parameter or not.
+	 * @param order_id integer that stores the id of the order we want to check its existence.
+	 * @return true if the order exists.
+	 */
+	//TODO: throw an exception
+	boolean orderExists(int order_id);
+	
+	
 
 }
--- src/HospiCartJDBC/OrderManager.java
+++ src/HospiCartJDBC/OrderManager.java
@@ -1,5 +1,121 @@
 package HospiCartJDBC;
 
-public class OrderManager {
+import HospiCartInterfaces.IOrderManager;
+import HospiCartPOJOs.Client;
+import HospiCartPOJOs.Order;
+import HospiCartPOJOs.Status;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class OrderManager implements IOrderManager {
+    private Connection c;
+    private ConnectionManagerJDBC cm; //TODO delete this? it is not used
+
+    public OrderManager(ConnectionManagerJDBC cm) {
+        this.cm = cm;
+        this.c = cm.getConnection();
+    }
+
+    /**
+     * Method that creates a new order with the client it receives as parameter and setting the current date and the default status to the order.
+     * @param client object of Client that stores the user who made the order.
+     * @throws SQLException if there is a problem with the connection (it is closed or not properly initialised), if there is an error in the SQL query, if there is a mismatch between the data being inserted and the expected one, etc.
+     */
+    @Override
+    public Order createOrder(Client client) throws SQLException{
+        Order order = new Order(); //I create the Order object
+
+       //I initialise the order fields
+       order.setClient(client);
+       order.setOrderDate(Date.valueOf(LocalDate.now()));
+       order.setStatus(Status.ORDERED);
+
+       //I insert the order information that I have up to now
+       String sql = "INSERT INTO client_order (user_id, order_date, status) VALUES (?, ?, ?)";
+
+       //I create the order record and fetch the generated key (the id of the order)
+        try (PreparedStatement stmt = c.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            stmt.setInt(1, client.getUserId());
+            stmt.setDate(2, Date.valueOf(LocalDate.now())); // we set current date
+            stmt.setString(3, Status.ORDERED.name()); // we set the default status (ordered)
+
+            int affectedRows = stmt.executeUpdate();
+            if(affectedRows == 0) {
+                throw new SQLException("Creating order failed, no rows affected.");
+            }
+
+            //Now, I get the generated order id
+            try(var generatedKeys = stmt.getGeneratedKeys()) {
+                //We use var because it enables the compiler to infer the type of the variable from the initialization (in this case, var represents a result set)
+                if (generatedKeys.next()) {
+                    order.setOrderId(generatedKeys.getInt(1));
+                } else {
+                    throw new SQLException("Creating order failed, no ID obtained.");
+                }
+            }
+            stmt.close();
+            c.commit(); //we do this because we disabled the autocommit in the connection
+            return order;
+        } catch (SQLException e) {
+            //We "rollback" the transaction in case of error.
+            if(c != null){ //We make sure that c is not null as an error would be thrown when trying to rollback over a null object
+                try{
+                    c.rollback();
+                } catch(SQLException ex){
+                    throw new SQLException("Error during rollback: " + ex.getMessage(), ex);
+                }
+            }
+            throw new RuntimeException("Error creating order: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Order getOrderByID(int order_id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersByUser(int user_id) {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> getOrdersByOrderDate(LocalDate order_date) {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> getOrdersWithinDateRange(LocalDate startDate, LocalDate endDate) {
+        return List.of();
+    }
+
+    @Override
+    public void updateOrderStatus(int order_id, Status newStatus) {
+
+    }
+
+    @Override
+    public void deleteOrder(int order_id) {
+
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> getOrdersByStatus(Status status) {
+        return List.of();
+    }
+
+    @Override
+    public boolean orderExists(int order_id) {
+        return false;
+    }
 }
