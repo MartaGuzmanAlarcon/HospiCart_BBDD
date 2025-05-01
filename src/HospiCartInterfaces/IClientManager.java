47b0b5635931c8f24e7775d9e06a522294b9a03c
luciafandino <luciafandinogomez@gmail.com>
2025-05-01 14:06:28 +0200

    Interfaces IClientManager, IPaymentManager

---
+++ src/HospiCartInterfaces/IClientManager.java
@@ -0,0 +1,25 @@
+package HospiCartInterfaces;
+
+import java.util.List;
+
+import HospiCartPOJOs.Client;
+
+/**
+ * The interface IClientManager includes all CRUD operations on the Client POJO plus 
+ * a few finder helpers.
+ */
+public interface IClientManager {
+	public void insertClient (Client c); // Insert a new client in the database.
+	public void deleteClientbyId (Integer id) throws Exception;
+	
+	public List<Client> getListOfClients();
+	public Client getClientById (Integer c_id); // Look up a client by their primary key.
+	public Client getClientByEmail (String c_email) throws Exception; // Find a client by email (useful for login & uniqueness checks).
+	
+	// TODO ASK IF NAME, SURNAME AND EMAIL METHODS ARE REDUNDANT 
+	public void updateName (Integer id, String name) throws Exception;
+	public void updateSurname (Integer id, String surname) throws Exception;
+	public void updatePhoneNumber (Integer id, Integer phone_number) throws Exception;
+	public void updateAddress (Integer id,String address) throws Exception;
+	public void updateEmail(Integer id, String email) throws Exception;
+}
---
+++ src/HospiCartInterfaces/IPaymentManager.java
@@ -0,0 +1,21 @@
+package HospiCartInterfaces;
+import java.util.List;
+import HospiCartPOJOs.Payment;
+import HospiCartPOJOs.PaymentMethod;
+import HospiCartPOJOs.PaymentStatus;
+
+/**
+ * The interface IPaymentManager includes all CRUD operations and business methods for Payment entities.
+ */
+public interface IPaymentManager {
+	public void insertPayment(Payment p);
+	public void deletePaymentById(Integer paymentId) throws Exception;
+	
+	public List<Payment> getListOfPayments();
+	public Payment getPaymentById(Integer p_id);
+	
+	// TODO REVISE WHICH METHODS ARE REDUNDANT
+	public void updateAmount(Integer paymentId, Integer amount) throws Exception;
+	public void updatePaymentMethod(Integer paymentId, PaymentMethod method) throws Exception;
+	public void updatePaymentStatus(Integer paymentId, PaymentStatus status) throws Exception;
+}
